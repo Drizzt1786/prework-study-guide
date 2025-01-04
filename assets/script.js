c4ff8d7756a1177da67ebba3e2618a8d107fcd1b
Marcel Graham <marcelgraham817@gmail.com>
2025-01-04 10:11:45 -0700

    JAVA

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+const topics = ['HTML', 'CSS', 'GIT', 'JavaScript'];
+
+for(let x=0; x<topics.length; x++){
+
+    console.log(topics[x]);
+}
+
+
+
+
+
+
+
+
+
+
+/*if (topic === 'HTML'){
+
+    console.log("Lets's study HTML!");
+
+}else if (topic === 'CSS'){
+
+    console.log("Lets study CSS!");
+
+}else if (topic === 'GIT'){
+
+    console.log("Lets study GIT!");
+
+}else if (topic === 'JavaScript'){
+
+    console.log("Lets study Javascript!")
+
+}else{
+
+    console.log("Please try again!")
+}*/
+
+
